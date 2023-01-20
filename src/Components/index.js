4163670261967b8aeb52fe85e1c12ca5c4e2ccbb
sharad4 <sharad011989@gmail.com>
2023-01-20 15:53:13 +0530

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,12 @@
+import Analytics from "./Analytics";
+import Control from "./Control";
+import DashboardPreview from "./DashboardPreview";
+import Distribution from "./Distribution";
+import Feature from "./Feature";
+import Footer from "./Footer";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import STTop from "./STTop";
+
+export { Analytics, Control, DashboardPreview, Distribution, Feature,
+Footer, Home, Navbar, STTop};
---
+++ src/Images/index.js
@@ -0,0 +1,7 @@
+import dashboard from "./dashboard.png";
+import feature from "./feature.png";
+import onephone from "./onephone.png";
+import twophones from "./twophones.png";
+
+
+export { dashboard, feature, onephone, twophones};
